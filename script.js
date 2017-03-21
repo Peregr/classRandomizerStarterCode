5cd1a348dd9088358ffc0eec723f96ccec6c294d
Anahi Peregrina <aperegrina.student@hsfi.us>
2017-03-21 21:20:56 +0000

    the classmate randomizer

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-$(document).ready(function(){
+$(document).ready(function() {
+    var teacher = ["Nick", "Jenna", "Raf", "Mathew", "Michael"];
+    //console.log(teacher[idx])
 
+    $("#teacherButton").click(function() {  
+        var idx = Math.floor(Math.random() * teacher.length);
+        $("#teacherDisplay").html(teacher[idx])
+   });
 
+    var classmates = ["Isabela", "Gabby", "Jacky", "Felix", "Darli", "Lauren", "Zarra", "Pamela"];
+     $("#studentButton").click(function() {  
+        var idx = Math.floor(Math.random() * classmates.length);
+        $("#studentDisplay").html(classmates[idx])
+   });
 
 });
